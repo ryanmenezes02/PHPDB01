6dc8f744a49de0d5481175069acb3039ef64822e
Ryan Menezes <99756036+ryanmenezes02@users.noreply.github.com>
2022-04-08 16:45:59 -0300

    Terceira aula

---
+++ _footer.php
@@ -0,0 +1,15 @@
+</main>
+
+<footer>
+
+    <a href="/" title="Página Inicial"><i class="fa-solid fa-house-chimney"></i></a>
+    <div title="Todos os direitos reservados.">&copy; Copyright 2022 Vitugo.</div>
+    <a href="#top" title="Topo da Página"><i class="fa-solid fa-circle-up"></i></a>
+
+</footer>
+
+</div>
+
+</body>
+
+</html>
---
+++ modelo.php
@@ -0,0 +1,28 @@
+<?php
+
+//Inclue o cabeçalho da página
+require('_header.php');
+
+?>
+            <section>
+                <h2>
+                    Título da Página
+                </h2>
+                <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Voluptatem et iusto expedita deserunt nobis voluptates est nam ipsum cum doloremque. Ipsum consequuntur minus eum libero amet molestiae blanditiis quos quod.</p>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Magnam pariatur minima maiores inventore sit, repudiandae, deserunt delectus omnis molestias ipsum natus rem ullam mollitia alias asperiores est repellat quod culpa.</p>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Est ipsa et autem sequi? Inventore aliquam, quas et ducimus rerum ipsam impedit harum soluta voluptatem error, ex dolor unde. Earum, libero?</p>
+            </section>
+
+            <aside>
+                <h3>
+                    Lateral
+                </h3>
+                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Odit saepe</p>
+            </aside>
+
+<?php
+
+//Inclue o roda pé na página
+require('_footer.php');
+
+?>
