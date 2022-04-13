e2332c056c9212a5c198dfd60449b203a67a2635
Ryan Menezes <99756036+ryanmenezes02@users.noreply.github.com>
2022-04-13 16:37:17 -0300

    Quinta Aula

--- _config.php
+++ _config.php
@@ -14,4 +14,56 @@ $db = array(
     "username" => "root",
     "password" => ""
 );
+/**
+ * Linha de conexão com o banco de dados.
+ */
 $conn = new mysqli($db['hostname'], $db['username'], $db['password'], $db['database']);
+
+/**
+ * SQL de teste do banco de dados.
+ */
+$sql = "SELECT * FROM articles WHERE art_status = 'on'";
+
+/**
+ * Executa a query, armazenando o resultado em '$res'
+ */
+$res = $conn->query($sql);
+
+/**
+ * Loop para obter cada um dos registros.
+ * O método 'fetch_assoc()' retorna cada registro dentro de um array.
+ */
+while ($article = $res->fetch_assoc()) {
+
+    /**
+     * Exibindo valor da array no HTML.
+     */
+    echo '<pre>';
+    print_r($article);
+    echo '</pre>';
+}
+
+/**
+ * SQL de teste com o banco de dados.
+ * Usando HEREDOC.
+ */
+$sql = <<<SQL
+
+INSERT INTO contacts (
+    name,
+    email,
+    subject,
+    message
+) VALUES (
+    'Joca',
+    'joca@joca',
+    'Contato do Joca',
+    'Mensagem do Joca'
+);
+
+SQL;
+
+/**
+ * Executa a query, armazenando o resultado em '$res'
+ */
+$conn->query($sql);
---
+++ database/vitugo.sql
@@ -0,0 +1,105 @@
+-- Apaga o banco de dados caso exista.
+DROP DATABASE IF EXISTS vitugo;
+
+-- Cria o banco de dados
+CREATE DATABASE vitugo CHARACTER SET utf8 COLLATE utf8_general_ci;
+
+-- Seleciona banco de dados.
+USE vitugo;
+
+-- Cria tabela de usuários/autores.
+CREATE TABLE users (
+    user_id INT PRIMARY KEY AUTO_INCREMENT,
+    user_name VARCHAR(255),
+    user_email VARCHAR(255),
+    user_birth DATE,
+    user_photo VARCHAR(255),
+    user_date TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    user_profile TEXT,
+    user_type ENUM('user', 'author', 'admin') DEFAULT 'user',
+    user_status ENUM('on', 'off', 'deleted') DEFAULT 'on'
+);
+
+-- Cria tabela de artigos
+CREATE TABLE articles (
+    art_id INT PRIMARY KEY AUTO_INCREMENT,
+    art_title VARCHAR(127),
+    art_intro VARCHAR(255),
+    art_thumb VARCHAR(255),
+    art_content TEXT,
+    art_author INT,
+    art_date TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    art_status ENUM('on', 'off', 'deleted') DEFAULT 'on',
+    FOREIGN KEY (art_author) REFERENCES users (user_id)
+);
+
+-- Cria tabela de comentários nos artigos
+CREATE TABLE comments (
+    cmt_id INT PRIMARY KEY AUTO_INCREMENT,
+    cmt_article INT,
+    cmt_author INT,
+    cmt_date TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    cmt_status ENUM('on', 'off', 'deleted') DEFAULT 'on',
+    FOREIGN KEY (cmt_article) REFERENCES articles (art_id),
+    FOREIGN KEY (cmt_author) REFERENCES users (user_id)
+);
+
+-- Cria a tabela com contatos do site
+CREATE TABLE contacts (
+    id INT PRIMARY KEY AUTO_INCREMENT,
+    name VARCHAR(127),
+    email VARCHAR(255),
+    subject VARCHAR(255),
+    message TEXT,
+    date TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    status ENUM('on', 'off', 'deleted') DEFAULT 'on',
+);
+
+-- Insere dados em 'users'
+INSERT INTO users (
+    user_name,
+    user_email,
+    user_birth,
+    user_photo,
+    user_profile,
+    user_type
+) VALUES 
+(
+    'Setembrino Trocatapas',
+    'set@brino.com',
+    '1990-10-14',
+    'https://randomuser.me/api/portraits/lego/6.jpg',
+    'Programador, agricultor, enrolador, fazedor de nada.',
+    'author'
+),
+(
+    'Emegarda Sirineuza',
+    'emen@siri.com',
+    '2000-01-11',
+    'https://randomuser.me/api/portraits/lego/3.jpg',
+    'Cantora, compositora, arranjadora, arrumadora e operadora.',
+    'user'
+),
+(
+    'Eletricildo Chocante',
+    'eletro@choque.com',
+    '1981-05-02',
+    'https://randomuser.me/api/portraits/lego/5.jpg',
+    'Motorista, eletricista, ortopedista e horista.',
+    'admin'
+);
+
+-- Insere dados em 'articles'
+INSERT INTO articles (
+    art_title,
+    art_intro,
+    art_thumb,
+    art_content,
+    art_author
+) VALUES
+(
+    'Primeiro artigo'
+    'Como fazer alguma coisa que você não sabe fazer.'
+    'https://picsum.photos/100'
+    ''
+)
